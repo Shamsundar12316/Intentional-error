c1257de5afedc88e1d5d197d04fde6a3b9be9470
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:09:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// part 18 // question 22// international error
+// let listing: string[] = ["ali","ahmed","nadeem","sham","lal",]
+// console.log(listing[5])
+// correct error
+var listing = ["ali", "ahmed", "nadeem", "sham", "lal", "radai"];
+console.log(listing[0]);
+console.log(listing[2]);
+console.log(listing[3]);
+console.log(listing[4]);
